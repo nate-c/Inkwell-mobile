5ec23d1f1df35da187b866afc838324810f5f72c
sboamah <sboamah@stevens.edu>
2021-06-17 06:57:38 -0400

    Start login and registration pages

---
+++ lib/screens/login.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyLogin());
+
+
+class MyLogin extends StatefulWidget {
+  @override
+  MyLoginState createState() {
+    return MyLoginState();
+  }
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class MyLoginState extends State<MyLogin> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Scaffold(
+    body: Center(
+    key: _formKey,
+    child: Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Inkwell".toUpperCase(), style: TextStyle(fontSize: 30, ), textAlign: TextAlign.center,),
+          Text("Don't have an account? Register", style: TextStyle(fontSize: 15), textAlign: TextAlign.center,),
+       //TODO: Add redirection to registration page
+          _buildFormField('User Name'),
+          _buildFormField('Passcode'),
+          ElevatedButton(
+            onPressed: () {
+            },
+            child: Text('Log in'.toUpperCase()),
+          ),
+        ],
+      ),
+    )
+    )
+    );
+  }
+}
+
+@override
+_buildFormField(String formInput){
+   Container(
+          width: 300,
+          child: TextFormField(
+            decoration: InputDecoration(
+              hintText: formInput),
+          )
+          );
+}
---
+++ lib/screens/register.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyRegistration());
+
+class MyRegistration extends StatefulWidget {
+  @override
+  MyRegistrationState createState() {
+    return MyRegistrationState();
+  }
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+@override
+class MyRegistrationState extends State<MyRegistration> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return new Scaffold(
+    body: Center(
+    child: Form(
+      key: _formKey,
+      child: Column(
+          children:<Widget>[ 
+          Text("Inkwell".toUpperCase(), style: TextStyle(fontSize: 30, ), textAlign: TextAlign.center,),
+          Text("Don't have an account? Register", style: TextStyle(fontSize: 15), textAlign: TextAlign.center,),
+           //TODO: Add redirection to login page
+          _buildFormField("First Name"),
+          _buildFormField("Last Name"),
+          _buildFormField("User Name"),
+          _buildFormField("Passcode"),
+          ElevatedButton(
+            onPressed: () {
+              // Validate returns true if the form is valid, or false otherwise.
+            },
+            child: Text('Log in'.toUpperCase()),
+          ),
+        ],
+      ),
+    )
+    )
+    );
+  }
+}
+
+@override
+_buildFormField(String formInput){
+   Container(
+          width: 300,
+          child: TextFormField(
+            decoration: InputDecoration(
+              hintText: formInput),
+  // The validator receives the text that the user has entered.
+          )
+          );
+}
