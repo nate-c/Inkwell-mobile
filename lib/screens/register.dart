6f0b47f474e22260448de0cf390894acb9537408
Nate <ncox14@yahoo.com>
2021-06-18 03:21:31 -0400

    fixed null error showingup on register screen

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(MyRegistration());
 
 class MyRegistration extends StatefulWidget {
@@ -12,21 +13,31 @@ class MyRegistration extends StatefulWidget {
 // This class holds data related to the form.
 @override
 class MyRegistrationState extends State<MyRegistration> {
-
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
     return new Scaffold(
-    body: Center(
-    child: Form(
+        body: Center(
+            child: Form(
       key: _formKey,
       child: Column(
-          children:<Widget>[ 
-          Text("Inkwell".toUpperCase(), style: TextStyle(fontSize: 30, ), textAlign: TextAlign.center,),
-          Text("Don't have an account? Register", style: TextStyle(fontSize: 15), textAlign: TextAlign.center,),
-           //TODO: Add redirection to login page
+        children: <Widget>[
+          Text(
+            "Inkwell".toUpperCase(),
+            style: TextStyle(
+              fontSize: 30,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            "Don't have an account? Register",
+            style: TextStyle(fontSize: 15),
+            textAlign: TextAlign.center,
+          ),
+          //TODOS: Add redirection to login page
+          // FIX THIS:
           _buildFormField("First Name"),
           _buildFormField("Last Name"),
           _buildFormField("User Name"),
@@ -39,20 +50,16 @@ class MyRegistrationState extends State<MyRegistration> {
           ),
         ],
       ),
-    )
-    )
-    );
+    )));
   }
 }
 
 @override
-_buildFormField(String formInput){
-   Container(
-          width: 300,
-          child: TextFormField(
-            decoration: InputDecoration(
-              hintText: formInput),
-  // The validator receives the text that the user has entered.
-          )
-          );
+_buildFormField(String formInput) {
+  return Container(
+      width: 300,
+      child: TextFormField(
+        decoration: InputDecoration(hintText: formInput),
+        // The validator receives the text that the user has entered.
+      ));
 }
