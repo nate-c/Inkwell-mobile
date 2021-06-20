d566e053e1dcfd4cee73b19f1d5bc4ae0ba40a94
Nate <ncox14@yahoo.com>
2021-06-20 02:29:37 -0400

    added uri constants

---
+++ lib/constants/uriConstants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class UriConstants {
+  final String baseUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/';
+  final String authUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/auth/login/';
+  final String registerUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/auth/register/';
+  final String getTickerInfoUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getTickerInfo/';
+  final String getAllTickersUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getAllTickers/';
+  final String getFilteredTickersUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getMatchingTickers/';
+  final String executeTradeUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/trades/executeTrade/';
+  final String getTradesUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/trades/getTrades/';
+}
