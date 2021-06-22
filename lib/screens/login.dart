eb03c06ae2122bc4d226a503f1eccd8b7bd45162
Nate <ncox14@yahoo.com>
2021-06-22 00:38:09 -0400

    commenting out url launcher bc its causing a bug

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inkwell_mobile/screens/register.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
 import '../main.dart';
 
@@ -11,13 +11,14 @@ class MyLogin extends StatefulWidget {
   MyLoginState createState() {
     return MyLoginState();
   }
-    Widget build(BuildContext context) {
+
+  Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         textTheme: Theme.of(context).textTheme.apply(
-        bodyColor: Colors.white,
-        displayColor: Colors.white,
-        ),
+              bodyColor: Colors.white,
+              displayColor: Colors.white,
+            ),
       ),
       initialRoute: '/',
       routes: {
@@ -38,18 +39,18 @@ class MyLoginState extends State<MyLogin> {
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
     return Scaffold(
-      appBar: AppBar( 
-        backgroundColor: Colors.transparent,
-        actions: <Widget> [
-          IconButton(
-            color: Colors.blue,
-                onPressed: (){ 
-                    Navigator.pushNamed(context, '/');
-                    }, 
-                    icon: const Icon(Icons.arrow_back)),
-        ],
-      ),
-      backgroundColor: const Color(0xFF011240),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          actions: <Widget>[
+            IconButton(
+                color: Colors.blue,
+                onPressed: () {
+                  Navigator.pushNamed(context, '/');
+                },
+                icon: const Icon(Icons.arrow_back)),
+          ],
+        ),
+        backgroundColor: const Color(0xFF011240),
         body: Center(
             key: _formKey,
             child: Form(
@@ -65,24 +66,25 @@ class MyLoginState extends State<MyLogin> {
                     textAlign: TextAlign.center,
                   ),
                   InkWell(
-                  child: Text(
-                    "Don't have an account? Register.",
-                    style: TextStyle(fontSize: 15),
-                    textAlign: TextAlign.center),
+                    child: Text("Don't have an account? Register.",
+                        style: TextStyle(fontSize: 15),
+                        textAlign: TextAlign.center),
                     onTap: () => Navigator.pushNamed(context, '/register'),
                   ),
                   //TODO: Add redirection to registration page
                   _buildFormField('User Name', Icon(null)),
                   _buildFormField('Passcode', Icon(Icons.lock)),
                   ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Log in'.toUpperCase(),
-                    style: TextStyle(color: Colors.white),),
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF002179), 
-                      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20), 
-                    )
-                  ),
+                      onPressed: () {},
+                      child: Text(
+                        'Log in'.toUpperCase(),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF002179),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                      )),
                 ],
               ),
             )));
@@ -96,14 +98,13 @@ _buildFormField(String formInput, Icon iconName) {
       margin: new EdgeInsets.all(15),
       color: const Color(0xFF071A4A),
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-      child: TextFormField(
-        decoration: InputDecoration(
-          hintText: formInput, 
-          hintStyle: TextStyle(color: Color(0xFFF2F2F2)), 
-          suffixIcon: iconName,
-          border: InputBorder.none
-          ),
-        style: TextStyle(color: Colors.white, fontSize: 15),
-      )));
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+          child: TextFormField(
+            decoration: InputDecoration(
+                hintText: formInput,
+                hintStyle: TextStyle(color: Color(0xFFF2F2F2)),
+                suffixIcon: iconName,
+                border: InputBorder.none),
+            style: TextStyle(color: Colors.white, fontSize: 15),
+          )));
 }
