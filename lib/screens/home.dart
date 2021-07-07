b1e4708a0d88cd3f913097d6bce573887c9dda9a
Nate <ncox14@yahoo.com>
2021-07-07 11:45:00 -0400

    home page updates

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,6 @@
 import 'dart:html';
 
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
