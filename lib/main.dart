757895625b4c2f0b6afc9390414465835a6c9110
Nate <ncox14@yahoo.com>
2021-07-07 13:58:42 -0400

    added home page back to routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:inkwell_mobile/screens/login.dart';
 import 'package:inkwell_mobile/screens/register.dart';
+import 'package:inkwell_mobile/screens/home.dart';
 import 'package:provider/provider.dart';
 import 'utils/authentication.dart';
 
@@ -13,41 +14,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(
-          value: Authentication(),
+        providers: [
+          ChangeNotifierProvider.value(
+            value: Authentication(),
           )
-      ],
-    child: MaterialApp(
-      title: 'Inkwell',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        textTheme: Theme.of(context).textTheme.apply(
-              bodyColor: Colors.white,
-              displayColor: Colors.white,
-            ),
-      ),
-      home: MyHomePage(title: 'Inkwell'),
-      initialRoute: '/',
-      routes: {
-        // '/': (context) => MyHomePage(title: 'Inkwell'),
-        '/login': (context) => MyLogin(),
-        '/register': (context) => MyRegistration(),
-        // '/home': (context) => MyRegistration(),
-        // '/company': (context) => MyRegistration(),
-        // '/completedTrade': (context) => MyRegistration(),
-        // '/profile': (context) => MyRegistration(),
-      },
-    )
-    );
+        ],
+        child: MaterialApp(
+          title: 'Inkwell',
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            textTheme: Theme.of(context).textTheme.apply(
+                  bodyColor: Colors.white,
+                  displayColor: Colors.white,
+                ),
+          ),
+          home: MyHomePage(title: 'Inkwell'),
+          initialRoute: '/',
+          routes: {
+            // '/': (context) => MyHomePage(title: 'Inkwell'),
+            '/login': (context) => MyLogin(),
+            '/register': (context) => MyRegistration(),
+            '/home': (context) => Home(),
+            // '/company': (context) => MyRegistration(),
+            // '/completedTrade': (context) => MyRegistration(),
+            // '/profile': (context) => MyRegistration(),
+          },
+        ));
   }
 }
 
