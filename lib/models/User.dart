cb7b56219e2d5fcac246c7db00e1d3f0bff7d31d
Nate <ncox14@yahoo.com>
2021-07-07 12:00:17 -0400

    update user

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -3,16 +3,19 @@ class User {
   final String firstName;
   final String lastName;
   final String username;
-  final String amount;
+  int amount;
+  int accountId;
 
-  User(this.userId, this.firstName, this.lastName, this.username, this.amount);
+  User(this.userId, this.firstName, this.lastName, this.username, this.amount,
+      this.accountId);
 
   User.fromJson(Map<String, dynamic> json)
       : userId = json['user_id'],
         firstName = json['first_name'],
         lastName = json['last_name'],
         username = json['username'],
-        amount = json['amount'];
+        amount = json['amount'],
+        accountId = json['account_id'];
 
   Map<String, dynamic> toJson() => {
         'user_id': userId,
@@ -20,5 +23,6 @@ class User {
         'last_name': lastName,
         'username': username,
         'amount': amount,
+        'account_id': accountId,
       };
 }
