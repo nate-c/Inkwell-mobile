09b742185fad51feda3c20ebd14610fe598d8e9f
Nate <ncox14@yahoo.com>
2021-07-16 17:36:54 -0400

    updated uri constants

--- lib/constants/uriConstants.dart
+++ lib/constants/uriConstants.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
 
 class UriConstants {
-  static final String baseUri =
+  static final useProdUri = false;
+  static final prodUri =
       'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/';
-  static final String authUri =
-      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/auth/login/';
-  static final String registerUri =
-      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/auth/register/';
-  static final String getTickerInfoUri =
-      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getTickerInfo/';
-  static final String getAllTickersUri =
-      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getAllTickers/';
+  static final localUri = 'http://localhost:8080/';
+  static final String baseUri = useProdUri ? prodUri : localUri;
+  static final String authUri = baseUri + 'auth/login/';
+  static final String registerUri = baseUri + 'auth/register/';
+  static final String getTickerInfoUri = baseUri + 'search/getTickerInfo/';
+  static final String getAllTickersUri = baseUri + 'search/getAllTickers/';
   static final String getFilteredTickersUri =
-      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getMatchingTickers/';
-  static final String executeTradeUri =
-      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/trades/executeTrade/';
-  static final String getTradesUri =
-      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/trades/getTrades/';
+      baseUri + 'search/getMatchingTickers/';
+  static final String executeTradeUri = baseUri + 'trades/executeTrade/';
+  static final String getTradesUri = baseUri + 'trades/getTrades/';
   static final String getAddMoneyUri =
-      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/accounts/updateAccountBalance';
-  static final String getUserInvestments =
-      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/accounts/getAllInvestments';
+      baseUri + 'accounts/updateAccountBalance';
+  static final String getUserInvestmentsUri =
+      baseUri + 'accounts/getAllInvestments';
 }
