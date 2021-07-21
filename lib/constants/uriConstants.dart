1bb9712c8763ae73c017c61109cffc629954eae1
Nate <ncox14@yahoo.com>
2021-07-21 15:26:11 -0400

    corrected uri constants

--- lib/constants/uriConstants.dart
+++ lib/constants/uriConstants.dart
@@ -1,21 +1,62 @@
 import 'package:flutter/material.dart';
 
 class UriConstants {
-  static final useProdUri = false;
-  static final prodUri =
+  // static final useProdUri = true;
+  // static final prodUri =
+  //     'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/';
+  // static final localUri = 'http://localhost:8080/';
+  // static final String baseUri = useProdUri ? prodUri : localUri;
+  // static final String authUri = baseUri + 'auth/login/';
+  // static final String registerUri = baseUri + 'auth/register/';
+  // static final String getTickerInfoUri = baseUri + 'search/getTickerInfo/';
+  // static final String getAllTickersUri = baseUri + 'search/getAllTickers/';
+  // static final String getFilteredTickersUri =
+  //     baseUri + 'search/getMatchingTickers/';
+  // static final String executeTradeUri = baseUri + 'trades/executeTrade/';
+  // static final String getTradesUri = baseUri + 'trades/getTrades/';
+  // static final String getAddMoneyUri =
+  //     baseUri + 'accounts/updateAccountBalance';
+  // static final String getUserInvestmentsUri =
+  //     baseUri + 'accounts/getAllInvestments';
+  // static String getAuthUri() {
+  //   print(baseUri + 'auth/login/');
+  //   return baseUri + 'auth/login/';
+  // }
+  static final String baseUri =
       'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/';
-  static final localUri = 'http://localhost:8080/';
-  static final String baseUri = useProdUri ? prodUri : localUri;
-  static final String authUri = baseUri + 'auth/login/';
-  static final String registerUri = baseUri + 'auth/register/';
-  static final String getTickerInfoUri = baseUri + 'search/getTickerInfo/';
-  static final String getAllTickersUri = baseUri + 'search/getAllTickers/';
+  static final String authUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/auth/login/';
+  static final String registerUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/auth/register/';
+  static final String getTickerInfoUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getTickerInfo/';
+  static final String getAllTickersUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getAllTickers/';
   static final String getFilteredTickersUri =
-      baseUri + 'search/getMatchingTickers/';
-  static final String executeTradeUri = baseUri + 'trades/executeTrade/';
-  static final String getTradesUri = baseUri + 'trades/getTrades/';
-  static final String getAddMoneyUri =
-      baseUri + 'accounts/updateAccountBalance';
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getMatchingTickers/';
+  static final String addMoneyUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/accounts/updateAccountBalance';
+  static final String executeTradeUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/trades/executeTrade/';
+  static final String getTradesUri =
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/trades/getTrades/';
   static final String getUserInvestmentsUri =
-      baseUri + 'accounts/getAllInvestments';
+      'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/accounts/getAllInvestments';
 }
+
+  // final String baseUri =
+  //     'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/';
+  // final String authUri =
+  //     'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/auth/login/';
+  // final String registerUri =
+  //     'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/auth/register/';
+  // final String getTickerInfoUri =
+  //     'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getTickerInfo/';
+  // final String getAllTickersUri =
+  //     'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getAllTickers/';
+  // final String getFilteredTickersUri =
+  //     'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/search/getMatchingTickers/';
+  // final String executeTradeUri =
+  //     'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/trades/executeTrade/';
+  // final String getTradesUri =
+  //     'http://inkwellservices-env.eba-k5w7dcu7.us-east-2.elasticbeanstalk.com/trades/getTrades/';
