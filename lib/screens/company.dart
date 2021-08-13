b9fd800ebcef29c9af64b5708cf278b38334c1b3
Nate <ncox14@yahoo.com>
2021-08-13 01:00:32 -0400

    removing old scaffolding code

--- lib/screens/company.dart
+++ lib/screens/company.dart
@@ -75,7 +75,6 @@ class CompanyState extends State<Company> {
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
           backgroundColor: ColorConstants.appBarBackground,
-          automaticallyImplyLeading: false,
           actions: <Widget>[
             PopupMenuButton<int>(
               color: ColorConstants.expandable,
@@ -185,35 +184,6 @@ class CompanyState extends State<Company> {
                     label: const Text('View Details'),
                     backgroundColor: Color.fromARGB(0, 255, 0, 0),
                   ),
-                  Container(
-                      width: 300,
-                      margin: new EdgeInsets.all(15),
-                      color: ColorConstants.textFieldBox,
-                      child: Padding(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                          child: TextFormField(
-                            controller: _searchController,
-                            onEditingComplete: () async {
-                              search();
-                              hideKeyboard(context);
-                            },
-                            decoration: InputDecoration(
-                                labelText:
-                                    "Search by company names or ticker...",
-                                labelStyle: TextStyle(
-                                    color: ColorConstants.textInTextField),
-                                border: InputBorder.none),
-                            style: TextStyle(
-                                color: ColorConstants.bodyText, fontSize: 15),
-                          ))),
-                  Container(
-                      height: 200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [...getTextWidgets()],
-                      )),
                   Container(
                     height: 50,
                   ),
