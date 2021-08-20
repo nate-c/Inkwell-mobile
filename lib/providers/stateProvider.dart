3a76646286094d0b891b11d63bd690383b72a27f
Nate <ncox14@yahoo.com>
2021-08-20 03:14:17 -0400

    removed unneeded packages from state provider

--- lib/providers/stateProvider.dart
+++ lib/providers/stateProvider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class StateProvider with ChangeNotifier {
   String _selectedCompanyTicker = '';
