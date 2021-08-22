764bd6de8364c2fb4a2445aed781d51c7b9a992c
Nate <ncox14@yahoo.com>
2021-08-22 01:40:28 -0400

    added assets constants class

---
+++ lib/constants/assetConstants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AssetConstants {
+  static final String logoPath = 'lib/images/ink_logo.png';
+  static final String demoChartPath = 'lib/images/sample_stock_chart.png';
+}
