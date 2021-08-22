85352d1c8fa90e2d30caf47966036d10ff064713
Nate <ncox14@yahoo.com>
2021-08-22 16:55:11 -0400

    added trade completion arguments

--- lib/models/routeArguments.dart
+++ lib/models/routeArguments.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:inkwell_mobile/models/investmentObject.dart';
 
 class CompanyScreenArguments {
-  final InvestmentObject? investmentObject;
+  final InvestmentObject investmentObject;
   // final String ticker;
   // final int shares;
 
   // CompanyScreenArguments(this.ticker, this.shares, this.investmentObject);
   CompanyScreenArguments(this.investmentObject);
+}
+
+class TradeCompletionScreenArguments {
+  final InvestmentObject investmentObject;
+  final String tradeType;
+
+  TradeCompletionScreenArguments(this.investmentObject, this.tradeType);
 }
