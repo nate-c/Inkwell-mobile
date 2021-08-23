1d4dbcbe776c45b042b18e215da279e3ddfb0022
Nate <ncox14@yahoo.com>
2021-08-23 15:05:22 -0400

    changed int to future

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -117,7 +117,7 @@ class MyHomeState extends State<Home> {
     }
   }
 
-  int setAccountId(String id) async {
+  Future setAccountId(String id) async {
     storage.write(key: "account_id", value: id);
     return 0;
   }
