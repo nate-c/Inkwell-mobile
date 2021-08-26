2d31e00271f09b6a02eabd2325dae8281013280e
Nate <ncox14@yahoo.com>
2021-08-26 03:02:59 -0400

    removed company state

--- lib/screens/company.dart
+++ lib/screens/company.dart
@@ -32,7 +32,7 @@ class Company extends StatelessWidget {
     var io = args.investmentObject;
     var shares = io.shares;
     bool hasShares = io.shares > 0;
-    void sellStock(){
+    void sellStock() {
       var newInvestment = new InvestmentObject(
           shares: io.shares,
           ticker: io.ticker,
@@ -42,7 +42,7 @@ class Company extends StatelessWidget {
           arguments: TradeCompletionScreenArguments(newInvestment, 'SELL'));
     }
 
-    void buyStock() async{
+    void buyStock() async {
       var newInvestment = new InvestmentObject(
           shares: io.shares,
           ticker: io.ticker,
@@ -194,98 +194,3 @@ class Company extends StatelessWidget {
                 ]))));
   }
 }
-
-// @override
-// class CompanyState extends State<Company> {
-//   // final User _user;
-//   String? _token;
-//   // final int _amount;
-
-//   // Array<String> _searchResults;
-//   // []TickerSearchObject _results;
-//   // final String _searchValue;
-//   //TODO: add function that changes value on homepage
-//   final TextEditingController _searchController = TextEditingController();
-//   final storage = new FlutterSecureStorage();
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     // print(args);
-//     // getUserInvestments();
-//     setInitialStateVariables();
-//     // getAccountInfo();
-//   }
-
-//   setInitialStateVariables() async {
-//     // var amount = await storage.read(key: 'amount');
-//     var token = await storage.read(key: 'jwt');
-
-//     _token = token.toString();
-//     setState(() {});
-//   }
-
-//   Widget build(BuildContext context) {
-//     return new Scaffold(
-//         resizeToAvoidBottomInset: false,
-//         appBar: AppBar(
-//           backgroundColor: ColorConstants.appBarBackground,
-//           actions: <Widget>[
-//             PopupMenuButton<int>(
-//               color: ColorConstants.expandable,
-//               icon: Icon(Icons.menu),
-//               itemBuilder: (context) => [
-//                 PopupMenuItem<int>(
-//                     value: 0,
-//                     child: Text(
-//                       "Home",
-//                       style: TextStyle(color: ColorConstants.bodyText),
-//                     )),
-//                 PopupMenuItem<int>(
-//                     value: 1,
-//                     child: Text(
-//                       "View Portfolio",
-//                       style: TextStyle(color: ColorConstants.bodyText),
-//                     )),
-//                 PopupMenuItem<int>(
-//                     value: 2,
-//                     child: Text(
-//                       "Add Money",
-//                       style: TextStyle(color: ColorConstants.bodyText),
-//                     )),
-//               ],
-//               onSelected: (item) => SelectedItem(context, item),
-//             ),
-//             // IconButton(
-//             // alignment: Alignment.centerRight,
-//             // color: ColorConstants.bodyText,
-//             // onPressed: () {
-//             //   Navigator.pushNamed(context, '/myprofile');
-//             // },
-//             // icon: const Icon(Icons.person),)
-//           ],
-//         ),
-//         backgroundColor: ColorConstants.background,
-//         body: Container(
-//             color: ColorConstants.background,
-//             // height: 100,
-//             margin: EdgeInsets.all(15), //originally 24
-//             // padding: EdgeInsets.only(top: 0),
-//             alignment: Alignment.center,
-//             child: Center(
-//                 // widthFactor: 100,
-//                 child: Column(
-//                     mainAxisAlignment: MainAxisAlignment.start,
-//                     crossAxisAlignment: CrossAxisAlignment.stretch,
-//                     children: [
-//                   Container(
-//                     height: 50,
-//                     child: Text('texe'),
-//                   ),
-//                   Container(
-//                     margin: new EdgeInsets.all(15),
-//                     color: ColorConstants.textFieldBox,
-//                   ),
-//                 ]))));
-//   }
-// }
